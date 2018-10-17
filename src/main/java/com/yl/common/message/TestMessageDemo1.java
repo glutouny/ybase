8866f4a9dd23b18e7100afa6f1398fd4c85be00b
yangli <li.yang01@hand-china.com>
2018-10-17 08:46:01 +0800

    添加短信测试

---
+++ src/main/java/com/yl/common/message/TestMessageDemo1.java
@@ -0,0 +1,99 @@
+package com.yl.common.message;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author 杨黎
+ * @Title
+ * @description 网易云验证码测试
+ * @DATE 2018/10/16  18:56
+ */
+public class TestMessageDemo1 {
+
+    //发送验证码的请求路径URL
+    private static final String
+            SERVER_URL="https://api.netease.im/sms/sendcode.action";
+    //网易云信分配的账号，请替换你在管理后台应用下申请的Appkey
+    private static final String
+            APP_KEY="15c59ffc1aede91109d4d336f9e86582";
+    //网易云信分配的密钥，请替换你在管理后台应用下申请的appSecret
+    private static final String APP_SECRET="3ec31d1c8cc6";
+    //随机数
+    private static final String NONCE="54088";
+    //短信模板ID
+    private static final String TEMPLATEID="9334231";
+    //手机号
+    private static final String MOBILE="15827556413";
+    //验证码长度，范围4～10，默认为4
+    private static final String CODELEN="6";
+
+    public static void main(String[] args) throws Exception {
+
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost(SERVER_URL);
+        String curTime = String.valueOf((new Date()).getTime() / 1000L);
+        /*
+         * 参考计算CheckSum的java代码，在上述文档的参数列表中，有CheckSum的计算文档示例
+         */
+        String checkSum = sha1(APP_SECRET+NONCE+curTime);
+
+        // 设置请求的header
+        httpPost.addHeader("AppKey", APP_KEY);
+        httpPost.addHeader("Nonce", NONCE);
+        httpPost.addHeader("CurTime", curTime);
+        httpPost.addHeader("CheckSum", checkSum);
+        httpPost.addHeader("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        // 设置请求的的参数，requestBody参数
+        List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+        /*
+         * 1.如果是模板短信，请注意参数mobile是有s的，详细参数配置请参考“发送模板短信文档”
+         * 2.参数格式是jsonArray的格式，例如 "['13888888888','13666666666']"
+         * 3.params是根据你模板里面有几个参数，那里面的参数也是jsonArray格式
+         */
+        nvps.add(new BasicNameValuePair("templateid", TEMPLATEID));
+        nvps.add(new BasicNameValuePair("mobile", MOBILE));
+        nvps.add(new BasicNameValuePair("codeLen", CODELEN));
+
+        httpPost.setEntity(new UrlEncodedFormEntity(nvps, "utf-8"));
+
+        // 执行请求
+        HttpResponse response = httpClient.execute(httpPost);
+        /*
+         * 1.打印执行结果，打印结果一般会200、315、403、404、413、414、500
+         * 2.具体的code有问题的可以参考官网的Code状态表
+         */
+        System.out.println(EntityUtils.toString(response.getEntity(), "utf-8"));
+
+    }
+    //对字符串进行SHA1哈希散列运算
+    public static String sha1(String data) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA1");
+        md.update(data.getBytes());
+        StringBuffer buf = new StringBuffer();
+        byte[] bits = md.digest();
+        for(int i=0;i<bits.length;i++){
+            int a = bits[i];
+            if(a<0){
+                a+=256;
+            }
+            if(a<16){
+                buf.append("0");
+            }
+            buf.append(Integer.toHexString(a));
+        }
+        return buf.toString();
+    }
+}
---
+++ src/main/java/com/yl/enums/PlatformType.java
@@ -0,0 +1,10 @@
+package com.yl.enums;
+
+/**
+ * @author 杨黎
+ * @Title   PlatformType
+ * @description 平台类型
+ * @DATE 2018/10/16  9:21
+ */
+public enum PlatformType {
+}
